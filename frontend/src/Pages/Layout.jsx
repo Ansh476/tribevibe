c94ab06bc9ba7c2d9463ec1ac310a9bfe776fc92
Ansh476 <115245771+Ansh476@users.noreply.github.com>
2024-10-07 00:53:43 +0530

    Update Layout.jsx

--- frontend/src/Pages/Layout.jsx
+++ frontend/src/Pages/Layout.jsx
@@ -3,12 +3,14 @@ import {Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 // import Navbar from '../components/Header';
+import { AuthContext } from '../components/authentication/Authcontext';
 
 
 const Layout = () => {
+  const { isLoggedIn, logout } = useContext(AuthContext);
   return (
     <>
-    <Header/>
+    <Header isLoggedIn={isLoggedIn} handleLogout={logout} />
     <Outlet/>
     <Footer/>
     </>
