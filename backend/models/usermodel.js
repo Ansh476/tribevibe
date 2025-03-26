1bc750c51244c10f19a00520207d518b622544a6
Ansh476 <115245771+Ansh476@users.noreply.github.com>
2025-03-27 00:15:43 +0530

    Update usermodel.js

--- backend/models/usermodel.js
+++ backend/models/usermodel.js
@@ -29,6 +29,10 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
+    interest: {
+        type: [String],
+        required: true
+    },
     otp: {
         type: String,
         required: false,
